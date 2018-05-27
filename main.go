d4c3bd163261faf95691cb654fa3ade82c60ba33
Kamaropoulos <kamaropoulos@outlook.com>
2018-05-27 20:27:16 +0300

    Changed static path registration to match the latest Goctapus API

--- main.go
+++ main.go
@@ -15,24 +15,31 @@ func main() {
 	goctapus.ConnectDB("goapp")
 	goctapus.Migrate(goctapus.Databases["goapp"], "./models/tasks.sql")
 
-	goctapus.AddStatic("/", "public/index.html")
+	goctapus.AddStatic(goctapus.Route{
+		Path: "/",
+		File: "public/index.html",
+		Rate: 10,
+	})
 
 	goctapus.AddEndpoint(goctapus.Route{
 		Method:  "GET",
 		Path:    "/tasks",
 		Handler: handlers.GetTasks(goctapus.Databases["goapp"]),
+		Rate:    10,
 	})
 
 	goctapus.AddEndpoint(goctapus.Route{
 		Method:  "PUT",
 		Path:    "/tasks",
 		Handler: handlers.PutTask(goctapus.Databases["goapp"]),
+		Rate:    10,
 	})
 
 	goctapus.AddEndpoint(goctapus.Route{
 		Method:  "DELETE",
 		Path:    "/tasks/:id",
 		Handler: handlers.DeleteTask(goctapus.Databases["goapp"]),
+		Rate:    10,
 	})
 
 	goctapus.Start()
